68ad25ee7e679dc9339f079aeb3dc6234f831864
Reddy Tintaya <aaabeeelooon@gmail.com>
2021-08-08 11:24:49 -0400

    cambiar app de huellas

--- huella_window.py
+++ huella_window.py
@@ -1,11 +1,11 @@
 import tkinter as tk  # python 3
-from tkinter import font  as tkfont  # python 3
+from tkinter import font as tkfont  # python 3
 from PIL import Image, ImageTk
 from network import Network
 from tkinter import messagebox
 from tkinter.ttk import Progressbar
 from tkinter import HORIZONTAL
-from CurrentUserPersistance import User,Dedo, set_user
+from CurrentUserPersistance import User, Dedo, set_user
 
 genero_aux = "Hombre"
 
@@ -17,7 +17,8 @@ class TakeHuella(tk.Frame):
 
         def check(field):
             if field is None or len(field) < 2:
-                tk.messagebox.showerror(title="Formulario incompleto", message="Formulario incompleto")
+                tk.messagebox.showerror(
+                    title="Formulario incompleto", message="Formulario incompleto")
 
         self.current_user.name = self.name.get()
         self.current_user.lastName = self.last_name.get()
@@ -38,14 +39,16 @@ class TakeHuella(tk.Frame):
             self.current_user.primer_analisis()
             self.current_user.calculate_d10()
             self.current_user.calculate_sqtl()
-            recomendacion1, recomendacion2 = User.search_in_table(self.current_user.d10, self.current_user.sqtl, self.current_user.tabla)
+            recomendacion1, recomendacion2 = User.search_in_table(
+                self.current_user.d10, self.current_user.sqtl, self.current_user.tabla)
             # self.current_user.segundo_analisis()
             self.current_user.recomendacion1 = recomendacion1
             self.current_user.recomendacion2 = recomendacion2
             Network.saveUser(self.current_user)
             self.resultsWindow()
         else:
-            tk.messagebox.showerror(title="Formulario incompleto", message="No se ingresaron todas las huellas")
+            tk.messagebox.showerror(
+                title="Formulario incompleto", message="No se ingresaron todas las huellas")
 
     def __init__(self, parent, controller):
         self.current_user = User()
@@ -70,14 +73,20 @@ class TakeHuella(tk.Frame):
         sep = 40
         piv = 140 + 30
         x = 45
-        y = lambda x: piv + (x * sep)
-        tk.LabelFrame(self, bg=orange, height=319, width=325).place(x=30, y=140)
-        tk.Label(self, text="Datos del Atleta", font=font, bg=orange).place(x=x, y=piv)
-        tk.Label(self, text="Nombres:", font=font, bg=orange).place(x=x, y=y(1))
-        tk.Label(self, text="Apellidos:", font=font, bg=orange).place(x=x, y=y(2))
+        def y(x): return piv + (x * sep)
+        tk.LabelFrame(self, bg=orange, height=319,
+                      width=325).place(x=30, y=140)
+        tk.Label(self, text="Datos del Atleta",
+                 font=font, bg=orange).place(x=x, y=piv)
+        tk.Label(self, text="Nombres:", font=font,
+                 bg=orange).place(x=x, y=y(1))
+        tk.Label(self, text="Apellidos:", font=font,
+                 bg=orange).place(x=x, y=y(2))
         tk.Label(self, text="CI:", font=font, bg=orange).place(x=x, y=y(3))
-        tk.Label(self, text="Fecha Nac:", font=font, bg=orange).place(x=x, y=y(4))
-        tk.Label(self, text="Telefono:", font=font, bg=orange).place(x=x, y=y(5))
+        tk.Label(self, text="Fecha Nac:", font=font,
+                 bg=orange).place(x=x, y=y(4))
+        tk.Label(self, text="Telefono:", font=font,
+                 bg=orange).place(x=x, y=y(5))
 
         tk.Label(self, text="Genero:", font=font, bg=orange).place(x=x, y=y(6))
         xEntry = 150
@@ -175,12 +184,14 @@ class TakeHuella(tk.Frame):
     def open_app(self):
         import os
         # only works for windows
-        os.startfile("{}\main.exe".format(os.getcwd()))
+        # os.startfile("{}\main.exe".format(os.getcwd()))
+        os.startfile("C:\IMAGEN\Enrollment.exe".format(os.getcwd()))
 
     def set_btn_valid(self, button, newWindow, field: Dedo):
         self.setIsLoading(True)
         from tkinter.filedialog import askopenfilename
-        filename = askopenfilename()  # show an "Open" dialog box and return the path to the selected file
+        # show an "Open" dialog box and return the path to the selected file
+        filename = askopenfilename()
         print(filename)
         print("making the request...")
 
@@ -196,30 +207,42 @@ class TakeHuella(tk.Frame):
             field.distance = distance
         self.setIsLoading(False)
         if category is not None:
-            tk.messagebox.showinfo("Registro Huella", "La huella de tipo {} ha sido registrada".format(category))
+            tk.messagebox.showinfo(
+                "Registro Huella", "La huella de tipo {} ha sido registrada".format(category))
             button.configure(bg='green')
         else:
-            tk.messagebox.showerror("Registro Huella", "La huella no ha sido registrada")
+            tk.messagebox.showerror(
+                "Registro Huella", "La huella no ha sido registrada")
             button.configure(bg='red')
 
         # w = tk.Message(self, text="this is a relatively long message", width=50).pack()
 
         newWindow.destroy()
+
     def resultsWindow(self):
         newWindow = tk.Toplevel(self)
         newWindow.title("Resultados")
         newWindow.geometry("400x400")
         font = ("Arial", "12", "bold italic")
-        tk.Label(newWindow, text="RESUMEN DE CARACTERISTICAS", font=font).place(x=100, y=50)
-        tk.Label(newWindow, text="ARCOS:          {}".format(self.current_user.categ["arco"]), font=font).place(x=100, y=100)
-        tk.Label(newWindow, text="PRESILLAS:      {}".format(self.current_user.categ["presilla"]), font=font).place(x=100, y=150)
-        tk.Label(newWindow, text="VERTICILOS:     {}".format(self.current_user.categ["verticilo"]), font=font).place(x=100, y=200)
-        tk.Label(newWindow, text="D10:            {}".format(self.current_user.d10), font=font).place(x=100, y=250)
-        tk.Label(newWindow, text="SQTL:           {}".format(self.current_user.sqtl), font=font).place(x=100, y=300)
-        tk.Label(newWindow, text="Recomendacion 1:{}".format(self.current_user.recomendacion1), font=font).place(x=100, y=350)
-        tk.Label(newWindow, text="Recomendacion 2:{}".format(self.current_user.recomendacion2), font=font).place(x=100, y=400)
+        tk.Label(newWindow, text="RESUMEN DE CARACTERISTICAS",
+                 font=font).place(x=100, y=50)
+        tk.Label(newWindow, text="ARCOS:          {}".format(
+            self.current_user.categ["arco"]), font=font).place(x=100, y=100)
+        tk.Label(newWindow, text="PRESILLAS:      {}".format(
+            self.current_user.categ["presilla"]), font=font).place(x=100, y=150)
+        tk.Label(newWindow, text="VERTICILOS:     {}".format(
+            self.current_user.categ["verticilo"]), font=font).place(x=100, y=200)
+        tk.Label(newWindow, text="D10:            {}".format(
+            self.current_user.d10), font=font).place(x=100, y=250)
+        tk.Label(newWindow, text="SQTL:           {}".format(
+            self.current_user.sqtl), font=font).place(x=100, y=300)
+        tk.Label(newWindow, text="Recomendacion 1:{}".format(
+            self.current_user.recomendacion1), font=font).place(x=100, y=350)
+        tk.Label(newWindow, text="Recomendacion 2:{}".format(
+            self.current_user.recomendacion2), font=font).place(x=100, y=400)
 
-        tk.Button(newWindow, text="Ok", command=lambda :self.controller.show_frame("ResultFrame")).place(x=100, y=450)
+        tk.Button(newWindow, text="Ok", command=lambda: self.controller.show_frame(
+            "ResultFrame")).place(x=100, y=450)
 
     def openNewWindow(self, btn, field):
         newWindow = tk.Toplevel(self)
@@ -237,7 +260,8 @@ class TakeHuella(tk.Frame):
         bg.image = photoEmi
         bg.place(x=0, y=0)
 
-        tk.Button(newWindow, text="Capturar Huella", command=self.open_app).place(relx=0.5, rely=0.3, anchor=tk.CENTER)
+        tk.Button(newWindow, text="Capturar Huella", command=self.open_app).place(
+            relx=0.5, rely=0.3, anchor=tk.CENTER)
         tk.Button(newWindow, text="Aceptar y Continuar",
                   command=lambda: self.set_btn_valid(btn, newWindow, field)).place(
             relx=0.5,
