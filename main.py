6d4f3895b0cfef9823c2206edd370ea55a428af6
Reddy Tintaya <aaabeeelooon@gmail.com>
2021-10-02 08:54:24 -0400

    remove unused pages

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ class SampleApp(tk.Tk):
 
         self.frames = {}
         # allowed screens
-        for F in (MainWindow, StartPage, PageTwo, AcercaDeAutor, TakeHuella, ResultFrame):
+        for F in (MainWindow, AcercaDeAutor, TakeHuella, ResultFrame):
             page_name = F.__name__
             frame = F(parent=container, controller=self)
             self.frames[page_name] = frame
@@ -35,38 +35,6 @@ class SampleApp(tk.Tk):
         '''Show a frame for the given page name'''
         frame = self.frames[page_name]
         frame.tkraise()
-
-
-class StartPage(tk.Frame):
-
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        self.controller = controller
-        label = tk.Label(self, text="This is the start page", font=controller.title_font)
-        label.pack(side="top", fill="x", pady=10)
-
-        button2 = tk.Button(self, text="Go to Page Two",
-                            command=lambda: controller.show_frame("PageTwo"))
-        button3 = tk.Button(self, text="Go to Page Huella",
-                            command=lambda: controller.show_frame("TakeHuella"))
-        button4 = tk.Button(self, text="Go to Page Main",
-                            command=lambda: controller.show_frame("MainWindow"))
-
-        button4.pack()
-        button2.pack()
-        button3.pack()
-
-
-class PageTwo(tk.Frame):
-
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        self.controller = controller
-        label = tk.Label(self, text="This is page 2", font=controller.title_font)
-        label.pack(side="top", fill="x", pady=10)
-        button = tk.Button(self, text="Go to the start page",
-                           command=lambda: controller.show_frame("StartPage"))
-        button.pack()
 
 
 if __name__ == "__main__":
